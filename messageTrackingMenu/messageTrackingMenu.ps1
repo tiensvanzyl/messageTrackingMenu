8bbdb09153f8f132b364eda47a1ac93da71e7b71
tiensvanzyl <tiens@fromashes.co.za>
2018-05-03 20:24:25 +0200

    umm

--- messageTrackingMenu/messageTrackingMenu.ps1
+++ messageTrackingMenu/messageTrackingMenu.ps1
@@ -15,17 +15,19 @@
 
 function Show-Menu() {
 
-    Write-Host "================ Message Tracking - Choose your preferred option below ================" #-ForegroundColor Green
+    Write-Host "================ Message Tracking - Choose your preferred option below ================`n" -ForegroundColor Green
     
-    Write-Host "1: Track mail with no subject full. (Output to console)." 
-    Write-Host "2: Track mail with no subject full. (Output to text file)." #-ForegroundColor Blue
-    Write-Host "3: Track mail with subject full. (Output to console)."
-    Write-Host "4: Track mail with subject full. (Output to text file)." #-ForegroundColor Blue
-    Write-Host "5: Track mail with no subject delivered only. (Output to console)."
-    Write-Host "6: Track mail with no subject delivered only. (Output to text file)." #-ForegroundColor Blue
-    Write-Host "7: Track mail with no subject sender only. (Output to console)."
-    Write-Host "8: Track mail with no subject recipient only. (Output to console)." #-ForegroundColor Blue
-    Write-Host "Q: Press 'Q' to quit."
+    Write-Host "1: Track mail with no subject full. (Output to console).`n" 
+    Write-Host "2: Track mail with no subject full. (Output to text file).`n" 
+    Write-Host "3: Track mail with subject full. (Output to console).`n"
+    Write-Host "4: Track mail with subject full. (Output to text file).`n" 
+    Write-Host "5: Track mail with no subject delivered to recipient only. (Output to console).`n"
+    Write-Host "6: Track mail with no subject delivered to recipient only. (Output to text file).`n" 
+    Write-Host "7: Track mail with no subject sender only. (Output to console).`n"
+    Write-Host "8: Track mail with no subject sender only. (Output to text file).`n"
+    Write-Host "9: Track mail with no subject recipient only. (Output to console).`n" 
+    Write-Host "10: Track mail with no subject recipient only. (Output to text file).`n" 
+    Write-Host "Q: Press 'Q' to quit.`n"
     }
     
     do {
@@ -40,16 +42,110 @@ function Show-Menu() {
                 $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
                 $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
                 
-                #Message tracking script for full detail here:
-
-                Get-Alias
+                Get-TransportServer $Server | Get-MessageTrackingLog -Sender $Sender -recipient $Recipient -start $StartDate -End $EndDate -resultsize unlimited | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}}
+                
             } '2' {
                 cls
-                'You chose option #2'
-                Get-ChildItem
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Sender = Read-Host -Prompt "Enter the sender email address."
+                $Recipient = Read-Host -Prompt "Enter the recipient email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                $Output = Read-Host -Prompt "Enter the path for the text file output. (c:\temp\tracking.txt)"
+                
+                Get-TransportServer $Server | Get-MessageTrackingLog -Sender $Sender -recipient $Recipient -start $StartDate -End $EndDate -resultsize unlimited | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}} > $Output
+
+                Write-Host "The results have been saved to $Output" -ForegroundColor Green
+
             } '3' {
                 cls
-                'You chose option #3'
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Sender = Read-Host -Prompt "Enter the sender email address."
+                $Recipient = Read-Host -Prompt "Enter the recipient email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                $Subject = Read-Host -Prompt "Enter the subject."
+
+                Get-TransportServer $Server | Get-MessageTrackingLog -Sender $Sender -recipient $Recipient -MessageSubject $Subject -start $StartDate -End $EndDate -resultsize unlimited | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}}
+
+            }'4' {
+                cls
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Sender = Read-Host -Prompt "Enter the sender email address."
+                $Recipient = Read-Host -Prompt "Enter the recipient email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                $Subject = Read-Host -Prompt "Enter the subject."
+                $Output = Read-Host -Prompt "Enter the path for the text file output. (c:\temp\tracking.txt)"
+
+                Get-TransportServer $Server | Get-MessageTrackingLog -Sender $Sender -recipient $Recipient -MessageSubject $Subject -start $StartDate -End $EndDate -resultsize unlimited | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}} > $Output
+
+                Write-Host "The results have been saved to $Output" -ForegroundColor Green
+
+            }'5' {
+                cls
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Recipient = Read-Host -Prompt "Enter the recipient email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                
+                Get-TransportServer $Server | Get-MessageTrackingLog -recipient $Recipient -start $StartDate -End $EndDate -resultsize unlimited  | where {$_.eventid –eq “deliver”} | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}}
+
+            }'6' {
+                cls
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Recipient = Read-Host -Prompt "Enter the recipient email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                $Output = Read-Host -Prompt "Enter the path for the text file output. (c:\temp\tracking.txt)"
+
+                Get-TransportServer $Server | Get-MessageTrackingLog -recipient $Recipient -start $StartDate -End $EndDate -resultsize unlimited  | where {$_.eventid –eq “deliver”} | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}} > $Output
+
+                Write-Host "The results have been saved to $Output" -ForegroundColor Green
+
+            }'7' {
+                cls
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Sender = Read-Host -Prompt "Enter the sender email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                $Output = Read-Host -Prompt "Enter the path for the text file output. (c:\temp\tracking.txt)"
+
+                Get-TransportServer $Server | Get-MessageTrackingLog -Sender $Sender -start $StartDate -End $EndDate -resultsize unlimited  | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}}
+
+            }'8' {
+                cls
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Sender = Read-Host -Prompt "Enter the sender email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                $Output = Read-Host -Prompt "Enter the path for the text file output. (c:\temp\tracking.txt)"
+
+                Get-TransportServer $Server | Get-MessageTrackingLog -Sender $Sender -start $StartDate -End $EndDate -resultsize unlimited  | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}} > $Output
+
+                Write-Host "The results have been saved to $Output" -ForegroundColor Green
+
+            }'9' {
+                cls
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Recipient = Read-Host -Prompt "Enter the recipient email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                
+                Get-TransportServer $Server | Get-MessageTrackingLog -Recipient $Recipient -start $StartDate -End $EndDate -resultsize unlimited  | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}}
+
+            }'10' {
+                cls
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Recipient = Read-Host -Prompt "Enter the recipient email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                $Output = Read-Host -Prompt "Enter the path for the text file output. (c:\temp\tracking.txt)"
+
+                Get-TransportServer $Server | Get-MessageTrackingLog -Recipient $Recipient -start $StartDate -End $EndDate -resultsize unlimited  | select-object Timestamp,EventID,ServerHostname,Totalbytes,MessageSubject,Sender,ReturnPath,@{Name="Recipients";Expression={$_.recipients}} > $Output
+
+                Write-Host "The results have been saved to $Output" -ForegroundColor Green
+
             } 'q' {
                 return
             }
---
+++ messageTrackingMenu/messageTrackingMenu/messageTrackingMenu.ps1
@@ -0,0 +1,59 @@
+###########################################################################################
+#   A message tracking script with a menu to assist users with easy message tracking      #
+#   Author: Tiens van Zyl                                                                 #
+#   Email: tiens.vanzyl@gmail.com                                                         #
+#   Date:   31 April 2018                                                                 #
+#   Updated by:                                                                           #
+#   Updates:                                                                              #
+#   Version: 0.1                                                                          #
+#   External link:                                                                        #
+#   Description:                                                                          #        
+#       This script was written as an easy way to track messages in Microsoft Exchange    #
+#        for users and junior administrators.                                             #
+#                                                                                         #
+###########################################################################################
+
+function Show-Menu() {
+
+    Write-Host "================ Message Tracking - Choose your preferred option below ================" #-ForegroundColor Green
+    
+    Write-Host "1: Track mail with no subject full. (Output to console)." 
+    Write-Host "2: Track mail with no subject full. (Output to text file)." #-ForegroundColor Blue
+    Write-Host "3: Track mail with subject full. (Output to console)."
+    Write-Host "4: Track mail with subject full. (Output to text file)." #-ForegroundColor Blue
+    Write-Host "5: Track mail with no subject delivered only. (Output to console)."
+    Write-Host "6: Track mail with no subject delivered only. (Output to text file)." #-ForegroundColor Blue
+    Write-Host "7: Track mail with no subject sender only. (Output to console)."
+    Write-Host "8: Track mail with no subject recipient only. (Output to console)." #-ForegroundColor Blue
+    Write-Host "Q: Press 'Q' to quit."
+    }
+    
+    do {
+        Show-Menu
+        $input = Read-Host "Please make a selection"
+        switch ($input) {
+            '1' {
+                cls
+                $Server = Read-Host -Prompt "Enter the server  name or server wildcard for multiple servers i.e server0*"
+                $Sender = Read-Host -Prompt "Enter the sender email address."
+                $Recipient = Read-Host -Prompt "Enter the recipient email address."
+                $StartDate = Read-Host -Prompt "Enter the start date (mm/dd/yy)."
+                $EndDate = Read-Host -Prompt "Enter the end date (mm/dd/yy)."
+                
+                #Message tracking script for full detail here:
+
+                Get-Alias
+            } '2' {
+                cls
+                'You chose option #2'
+                Get-ChildItem
+            } '3' {
+                cls
+                'You chose option #3'
+            } 'q' {
+                return
+            }
+        }
+        pause
+    }
+        until ($input -eq 'q')
